0aab8ec542b2f9e887351ec067970da484f59543
Hyeonjun <hj0216hj12@naver.com>
2025-10-13 15:25:27 +0900

    loops/main.go v0.1 strconv package

--- week07/loops/main.go
+++ week07/loops/main.go
@@ -5,14 +5,33 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
+	// shadowing
+	// var int int = 99
+	// var b int = 8
+	//var fmt string = "inha"
+	//fmt.Println(int, b)
+	//fmt.Println(fmt)
+
+	fmt.Println("Enter a grade :")
 	r := bufio.NewReader(os.Stdin)
 	i, err := r.ReadString('\n')
-	//fmt.Println(err)
 	if err != nil {
-		log.Fatal(err) // report the error, exit the program
+		log.Fatal(err)
 	}
-	fmt.Println(i)
+
+	i = strings.TrimSpace(i)                // 문자열 주위에 붙은 공간 및 탭 키 등 제거
+	score, err := strconv.ParseFloat(i, 64) // 정리된 문자열을 실수타입으로 변환
+	var status string
+	if score >= 60 {
+		status = "passing"
+		//status := "passig" << 지역변수 괄호 안에서만 존재
+	} else {
+		status = "falling"
+	}
+	fmt.Println(score, status)
 }
