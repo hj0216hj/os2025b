3a1de66c4102d83f9ba354c2ce132f95497bbdee
Hyeonjun <hj0216hj12@naver.com>
2025-11-10 14:33:39 +0900

    week11 slice v0.3

--- week11/cmd/ch05-ex01/main.go
+++ week11/cmd/ch05-ex01/main.go
@@ -3,18 +3,13 @@ package main
 import "fmt"
 
 func main() {
-
-	//slice v0.1에서 for에 subject 변수이름을 바꿔야 작동함
-	//var subjects []string
-	//subjects = make([]string, 3)
-
-	//subjects := make([]string, 3)
-
-	//subjects[0] = "Go"
-	//subjects[2] = "Python"
-
-	subjects := []string{"Go", "", "Python"} // slice literal
+	subjects := [4]string{"Go", "Javascript", "Python", "Linux"} // slice literal
+	subjectSlice := subjects[1:3]                                // slicing
 	for _, subject := range subjects {
 		fmt.Println(subject)
+	}
+	fmt.Println("===========================")
+	for i := 0; i < len(subjectSlice); i++ {
+		fmt.Println(subjectSlice[i])
 	}
 }
