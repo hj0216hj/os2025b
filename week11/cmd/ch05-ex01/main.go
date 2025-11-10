436a151fee7bc009413515e24be186e1bcb7d178
Hyeonjun <hj0216hj12@naver.com>
2025-11-10 14:54:39 +0900

    week11 slice v0.4

--- week11/cmd/ch05-ex01/main.go
+++ week11/cmd/ch05-ex01/main.go
@@ -4,7 +4,9 @@ import "fmt"
 
 func main() {
 	subjects := [4]string{"Go", "Javascript", "Python", "Linux"} // slice literal
-	subjectSlice := subjects[1:3]                                // slicing
+	subjectSlice := subjects[:3]
+	subjects[0] = "Java"
+	//subjectSlice[0] = "Java" // << 슬라이스가 바뀌면 원본도 같이 바뀐다. 같은 곳을 바라보고 있다.
 	for _, subject := range subjects {
 		fmt.Println(subject)
 	}
