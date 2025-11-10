90aa5c287adebdc11f25758d4f01a938cd595de8
Hyeonjun <hj0216hj12@naver.com>
2025-11-10 14:23:15 +0900

    week11 slice v0.1

---
+++ week11/cmd/ch05-ex01/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var subject []string
+	subject = make([]string, 3)
+	subject[0] = "Go"
+	subject[2] = "Python"
+
+	for _, subject := range subject {
+		fmt.Println(subject)
+	}
+}
