c7a7d6d030193247d00a5911c417d9b858862f35
Hyeonjun <hj0216hj12@naver.com>
2025-11-10 14:28:22 +0900

    week11 slice v0.2

--- week11/cmd/ch05-ex01/main.go
+++ week11/cmd/ch05-ex01/main.go
@@ -4,12 +4,17 @@ import "fmt"
 
 func main() {
 
-	var subject []string
-	subject = make([]string, 3)
-	subject[0] = "Go"
-	subject[2] = "Python"
+	//slice v0.1에서 for에 subject 변수이름을 바꿔야 작동함
+	//var subjects []string
+	//subjects = make([]string, 3)
 
-	for _, subject := range subject {
+	//subjects := make([]string, 3)
+
+	//subjects[0] = "Go"
+	//subjects[2] = "Python"
+
+	subjects := []string{"Go", "", "Python"} // slice literal
+	for _, subject := range subjects {
 		fmt.Println(subject)
 	}
 }
