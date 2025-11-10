322e2a0812c653ffd09fe777bc14fefe575e86d6
Hyeonjun <hj0216hj12@naver.com>
2025-11-10 15:00:23 +0900

    week11 slice v0.5

--- week11/cmd/ch05-ex01/main.go
+++ week11/cmd/ch05-ex01/main.go
@@ -6,7 +6,7 @@ func main() {
 	subjects := [4]string{"Go", "Javascript", "Python", "Linux"} // slice literal
 	subjectSlice := subjects[:3]
 	subjects[0] = "Java"
-	//subjectSlice[0] = "Java" // << 슬라이스가 바뀌면 원본도 같이 바뀐다. 같은 곳을 바라보고 있다.
+	subjectSlice = append(subjectSlice, "Go") // append(subjectSlice, "Go", "Database") << 원본에 둘다 추가가 안됨
 	for _, subject := range subjects {
 		fmt.Println(subject)
 	}
